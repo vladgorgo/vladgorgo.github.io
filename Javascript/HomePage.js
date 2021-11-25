05167468a9bde106a485395b3fdfbe24cd7796e8
vladgorgo <gorgovanvlad@gmail.com>
2021-11-26 01:11:55 +0200

    3rd Commit

--- Javascript/HomePage.js
+++ Javascript/HomePage.js
@@ -104,7 +104,7 @@ function AddGameRight() {
 
         + '<div class="column2">'
         + '<br>'
-        + '<br>'
+        + '<br>' 
         + '<br>'
         + '<br>'
         + '<iframe width="560" height="315" src="https://www.youtube.com/embed/ergiG9zVtPw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
