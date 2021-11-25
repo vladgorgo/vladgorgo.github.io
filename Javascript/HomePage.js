b8cccc1831bb62c7695c4c5b95d2595443363f87
vladgorgo <gorgovanvlad@gmail.com>
2021-11-26 01:10:17 +0200

    2nd Commit

--- Javascript/HomePage.js
+++ Javascript/HomePage.js
@@ -9,12 +9,12 @@
 
 function AddHeader()
 {
-    var icon_width = 40; 
+    var icon_width = 60; 
     $('#Header').empty().append(
         ''
         +'<ul class="topnav">'
-        + '<li><a href="/Home">Home</a></li>'
-        + '<li><a href="/AboutUs">About Us</a></li>'
+        + '<li><a href="./"><img src="Images/lw-cropped.png" width="170" height="60"></a></li>'
+        //+ '<li><a href="/AboutUs" height="48">About Us</a></li>'
 
         + '<li class="right"><a href="https://www.facebook.com/ReplayStud" target="_blank"><img src="Images/Icons/facebook.png" width="' + icon_width + '" height="' + icon_width +'"></a></li>'
         + '<li class="right"><a href="https://www.instagram.com/replay.stud" target="_blank"><img  src="Images/Icons/instagram.png" width="' + icon_width + '" height="' + icon_width +'"></a></li>'
@@ -117,12 +117,12 @@ function AddGameRight() {
         + '<div class="column2">'
         + '<br>'
         + '<br>'
-        + '<a href="https://play.google.com/store/apps/details?id=com.ReplayStudio.Ballzing&gl=RO" target="_blank"> <img src="Images/Games/Ballzing.png" width="150" height="150"></a>'
+        + '<img src="Images/Games/Ballzing.png" width="150" height="150">'
         + '<p><h1>Ballzing</h1></p>'
         + '<p style="padding-right:10%">Ballzing is a new challange that will test your nerves.'
         + 'In order to beat this game you\'ll need to develop a very strong muscle memory and reflexes.'
         + 'Try to beat this game and win the race between you and your friends.</p>'
-        + '<a href="https://play.google.com/store/apps/details?id=com.ReplayStudio.Ballzing&gl=RO" target="_blank"> <img src="Images/Other/GooglePlay.png" width="250" height="75"></a>'
+        + '<a href="https://play.google.com/store/apps/details?id=com.ReplayStudio.Ballzing" target="_blank"> <img src="Images/Other/GooglePlay.png" width="250" height="75"></a>'
         + '</div>'
 
        
