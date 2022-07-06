c676d2a0fa5d380b02f5d5360c4a214614980f3a
vladgorgo <gorgovanvlad@gmail.com>
2022-07-06 18:03:52 +0300

    ABM 3

--- ABM/scripts.js
+++ ABM/scripts.js
@@ -16,7 +16,7 @@ function LoadPage(i){
 
     while(j<13)
     {
-        poze+='<div class="w3-col l3 m6 w3-margin-bottom"><div class="w3-display-container"><img src="img/'+(j+buffer)+'.jpeg" style="width:100%"></div></div>'
+        poze+='<div class="w3-col l3 m6 w3-margin-bottom"><div class="w3-display-container"><img src="img/'+(j+buffer)+'.jpeg" style="width:85%"></div></div>'
         j++;
     }
     $(".PhotoGallery").empty().append('<div class="w3-row-padding">'
